13d635574716c2f3b48b830d56afa2cf2dd5c6ea
larakii1 <abdelghafourelkanbi@gmail.com>
2019-10-14 10:29:09 +0200

    modification projet sicknesstorm , création de la class fight et de ses attributs et méthodes(vide) , ajout attribut et méthodes de la class game (vide)

---
+++ src/model/Fight.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Fight {
+		
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void startFigh(int nbAtk , int nbDef) {
+		
+	}
+	public void checkWinner(int atk , int defd) {
+		
+	}
+	public void dice(int nbDices) {
+		
+	}
+
+
+}
