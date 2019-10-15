6ada570663a96e68ae348120f5b402fc3b3ad4d5
sofiane <sofiane.nasri.1995@gmail.com>
2019-10-15 12:13:13 +0200

    method attack from the class Territory created and have to be tested

--- src/model/Territory.java
+++ src/model/Territory.java
@@ -47,6 +47,40 @@ public class Territory {
 		this.playerId = playerId;
 	}
 	
+	/**
+	 * 
+	 * @param territory
+	 * @param nbAttack
+	 * @param nbDefense
+	 * perform an attack to the Territory passed in parameters,
+	 * check if the Territory is a neighbor before the attack.
+	 * if there's only 1 pawn on the Territory attacked, perform a shifumi.
+	 * if the attacker get all pawn from the attacked territory, call conquerTerritory(Territory, Nbpawns)
+	 */
+	public void attack(Territory territory, int nbAttack, int nbDefense) {
+		if(territoryAdjacent.contains(territory.id)) {
+			Fight fight = new Fight();
+			ArrayList<Integer> resultats = fight.startFight(nbAttack, nbDefense);
+			System.out.println("Territoire voisin");
+			if(territory.getPawn() == 1) {
+				if(this.shifumi() == true) {
+					territory.setPawn(territory.getPawn() - resultats.get(0));
+					this.pawn = this.pawn - resultats.get(1);
+				}
+			}
+			else {
+				territory.setPawn(territory.getPawn() - resultats.get(0));
+				this.pawn = this.pawn - resultats.get(1);
+			}
+			if(territory.getPawn() <= 0) {
+				this.conquerTerritory(territory, resultats.get(0));
+			}
+		}
+		else {
+			System.out.println("Territoire non voisin");		
+			}
+	}
+	
 	public void conquerTerritory(){ // take the control of a territory.
 		
 	}
