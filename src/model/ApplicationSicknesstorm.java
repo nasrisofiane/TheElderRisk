7b7e3e9bf639110b7b88d084c528a13a6282355d
verbaaal <verbaaal@gmail.com>
2019-10-15 12:00:22 +0200

    shifumi conflict fixed

--- src/model/ApplicationSicknesstorm.java
+++ src/model/ApplicationSicknesstorm.java
@@ -4,15 +4,7 @@ public class ApplicationSicknesstorm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-	
-		
-		
-		Player player=new Player();
-	 player.shifumi();
-	
-=======
->>>>>>> 44b3e6605b5b3e06cea4846c4d94ef7e3e7828ea
+
 	}
 
 }
--- src/model/Territory.java
+++ src/model/Territory.java
@@ -55,9 +55,71 @@ public class Territory {
 		
 	}
 	
-	public void shifumi() {
+	public boolean shifumi() {
+		int min=1;
+		int max=3;
+			
 		
-	}
+			
+			int attacker = min + (int)(Math.random() * ((max - min) + 1));
+			
+			int defender = min + (int)(Math.random() * ((max - min) + 1));
+			
+		
+			
+		if( attacker == 1) { 	System.out.println("pierre");}
+		
+		if( attacker == 2) { 	System.out.println("feuille");}
+		
+		if( attacker == 3) { 	System.out.println("ciseaux");}
+		
+		
+			
+		if( defender == 1) { 	System.out.println("pierre");}
+		
+		if( defender == 2) { 	System.out.println("feuille");}
+		
+		if( defender == 3) { 	System.out.println("ciseaux");}
+		
+			
+		if( attacker==1 &&defender==1) {System.out.println("Recommencer");
+		this.shifumi();
+		}
+		
+		if( attacker==1 &&defender==2) {System.out.println("la feuille gagne");
+		return false;
+		}
+		
+		if( attacker==1 &&defender==3) {System.out.println("la pierre gagne");
+		return true;
+		}
+		
+		if( attacker==2 &&defender==1) {System.out.println("la feuille gagne");
+		return true;
+		}
+		
+		if( attacker==2 &&defender==2) {System.out.println("Recommencer");
+		this.shifumi();
+		}
+		
+		if( attacker==2 &&defender==3) {System.out.println("le ciseaux gagne");
+		return false;
+		}
+		if( attacker==3 &&defender==1) {System.out.println("la pierre gagne");
+		return false;
+		}
+		if(attacker==3 &&defender==2) {System.out.println("le ciseaux gagne");
+		return true;
+		}
+		if( attacker==3 &&defender==3) {System.out.println("Recommencer");
+		this.shifumi();
+		}
+		
+		return false;	
+			
+			
+			
+		}
 
 	
 }
