9cd47ada4fe620090473cba1012d0cb9411e66f1
sofiane <sofiane.nasri.1995@gmail.com>
2019-10-23 22:28:19 +0200

    React project npm added to node_modules

--- sicknesstormreact/src/App.js
+++ sicknesstormreact/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Formulaire from './formulaire';
+import BodyMap from './BodyMap';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <BodyMap/>
+        <Formulaire/>
       </div>
     );
   }
