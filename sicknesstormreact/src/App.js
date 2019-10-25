acfe322cf195b5551983e8ab7ac1f7cde83c91af
larakii1 <abdelghafourelkanbi@gmail.com>
2019-10-25 16:08:53 +0200

    configuration et test reussis de la requete 'post' et de la fonction initializegame

--- sicknesstormreact/src/App.js
+++ sicknesstormreact/src/App.js
@@ -4,12 +4,20 @@ import Formulaire from './formulaire';
 import BodyMap from './BodyMap';
 import AttackPhase from './AttackPhase';
 class App extends Component {
+  
+
+  handleChildUnmount(){
+    this.setState({renderChild: false});
+}
+
   render() {
+    
+
     return (
       <div className="App">
-        {/* <BodyMap/>
-        <Formulaire/> */}
-        <AttackPhase/>
+       {/* <BodyMap/> */}
+        <Formulaire />
+        {/* <AttackPhase/> */}
       </div>
     );
   }
